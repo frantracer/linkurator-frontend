5eda71f66fe4da6ab6d6d4e40cbca279975ad276
raguilarbecerra <raguilarbecerra@vistaprint.com>
2021-11-27 01:12:56 +0100

    Use buttons for menu

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -5,12 +5,12 @@ type MenuItemProps = {
 };
 
 export const MenuItem = (props: MenuItemProps) => (
-  <div
+  <button
     onClick={props.onClick}
-    className={`block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg hover:bg-blue-400 focus:outline-none focus:shadow-outline ${
+    className={`block px-4 py-2 mt-2 text-sm w-full text-left font-semibold text-gray-900 rounded-lg hover:bg-blue-400 focus:outline-none focus:shadow-outline ${
       props.selected ? "focus:bg-blue-400" : ""
     }`}
   >
     {props.title}
-  </div>
+  </button>
 );
