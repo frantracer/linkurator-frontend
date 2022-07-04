8816a720dd51a865fc5125d6c01744f859094bfd
frantracer <frantracer@gmail.com>
2022-07-04 19:05:38 +0200

    Disable mocks by default

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,9 +1,18 @@
-if (typeof window === "undefined") {
-  const { server } = require("./server");
-  server.listen({ onUnhandledRequest: "bypass" });
-} else {
-  const { worker } = require("./browser");
-  worker.start({ onUnhandledRequest: "bypass" });
+import {server} from "./server";
+import {worker} from "./browser";
+
+const ENABLE_MOCKS = false;
+
+if (ENABLE_MOCKS) {
+  if (typeof window === "undefined") {
+    const {server} = require("./server");
+    server.listen({onUnhandledRequest: "bypass"});
+    console.log("Server started");
+  } else {
+    const {worker} = require("./browser");
+    worker.start({onUnhandledRequest: "bypass"});
+    console.log("Worker started");
+  }
 }
 
 export {};
