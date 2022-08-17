0a1578b60e2cf4d48cc3df85fcf51c63d7947d14
frantracer <frantracer@gmail.com>
2022-08-17 02:02:16 +0200

    Display title and publish information in video cards

--- components/SubscriptionVideoCardGrid.tsx
+++ components/SubscriptionVideoCardGrid.tsx
@@ -10,14 +10,44 @@ type SubscriptionVideoCardGridProps = {
 const SubscriptionVideoCardGrid = (props: SubscriptionVideoCardGridProps) => {
   const cards = [];
 
+  const formatDate = (date: Date) => {
+    const difference = new Date().getTime() - date.getTime();
+    let value: number;
+    let unit: string;
+    if (difference < 3600000) {
+      value = Math.floor(difference / 60000);
+      unit = "minute";
+    } else if (difference < 86400000) {
+      value = Math.floor(difference / 3600000);
+      unit = "hour";
+    } else if (difference < 604800000) {
+      value = Math.floor(difference / 86400000);
+      unit = "day";
+    } else if (difference < 2419200000) {
+      value = Math.floor(difference / 604800000);
+      unit = "week";
+    } else if (difference < 31536000000) {
+      value = Math.floor(difference / 2419200000);
+      unit = "month";
+    } else {
+      value = Math.floor(difference / 31536000000);
+      unit = "year";
+    }
+    if (value !== 1) {
+      unit += "s";
+    }
+
+    return `${value} ${unit} ago`;
+  }
+
   if (props.subscription) {
     for (let i = 0; i < props.items.length; i++) {
       cards.push(
         <div className="m-4" key={i}>
           <VideoCard
             img={props.items[i].thumbnail}
-            name={props.subscription.name}
-            description={props.items[i].name}
+            name={props.items[i].name}
+            description={formatDate(props.items[i].published_at)}
             url={props.items[i].url}
           />
         </div>
@@ -26,7 +56,10 @@ const SubscriptionVideoCardGrid = (props: SubscriptionVideoCardGridProps) => {
   }
 
   return (
-    <div className="flex flex-row flex-wrap m-6">{cards}</div>
+    <div>
+      <div className="text-3xl text-center text-gray-800">{props.subscription ? props.subscription.name : ""}</div>
+      <div className="flex flex-row flex-wrap m-6">{cards}</div>
+    </div>
   )
 }
 
--- hooks/useSubscriptionItems.tsx
+++ hooks/useSubscriptionItems.tsx
@@ -8,10 +8,29 @@ export type SubscriptionItem = {
   name: string;
   url: string;
   thumbnail: string
+  published_at: Date;
 };
 
 interface SubscriptionItemsResponse {
   elements: SubscriptionItem[];
+}
+
+const mapJsonToSubscriptionItemsResponse = (json: Record<string, any>): SubscriptionItemsResponse => {
+  return {
+    elements: json.elements.map((element: Record<string, any>) => {
+      const published_at = new Date(element.published_at);
+      if (isNaN(published_at.getTime())) {
+        throw new Error("Published at is not a valid date");
+      }
+      return {
+        uuid: element.uuid,
+        name: element.name,
+        url: element.url,
+        thumbnail: element.thumbnail,
+        published_at: new Date(element.published_at)
+      };
+    })
+  };
 }
 
 const useSubscriptionItems = (subscription?: Subscription) => {
@@ -22,8 +41,11 @@ const useSubscriptionItems = (subscription?: Subscription) => {
       if (subscription) {
         try {
           const url = configuration.SUBSCRIPTIONS_URL + subscription.uuid + "/items";
-          const {data} = await axios.get<SubscriptionItemsResponse>(url, {withCredentials: true});
-          setSubscriptionsItems(data.elements);
+          const response = await axios.get(url, {withCredentials: true});
+          if (response.status === 200) {
+            const items = mapJsonToSubscriptionItemsResponse(response.data).elements;
+            setSubscriptionsItems(items);
+          }
         } catch (error: any) {
           console.error("Error retrieving subscriptions", error);
         }
