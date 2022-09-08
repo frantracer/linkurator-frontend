a1958d71db4b956fb9a77e4d34d912c644954f01
frantracer <frantracer@gmail.com>
2022-09-08 09:20:16 +0200

    Remove useless pages

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
-import "../mocks";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
