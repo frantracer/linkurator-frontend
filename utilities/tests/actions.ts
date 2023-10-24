daec8d0192d929339d2fbafb8b0b2f0f33d9b5e2
frantracer <frantracer@gmail.com>
2023-10-24 08:58:45 +0200

    Update packages

--- utilities/tests/actions.ts
+++
@@ -1,7 +0,0 @@
-import { fireEvent, waitFor, screen } from "@testing-library/dom";
-
-export const clickOnText = async (text: string) => {
-  await waitFor(() => {
-    fireEvent.click(screen.getByText(text));
-  });
-};
