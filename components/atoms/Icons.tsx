2f7ab258dba7a7dacca4c224967f57a66d56779f
frantracer <frantracer@gmail.com>
2024-03-09 17:23:44 +0100

    Add check icon

--- components/atoms/Icons.tsx
+++ components/atoms/Icons.tsx
@@ -215,3 +215,14 @@ export const CrossIcon = () => {
     </svg>
   )
 }
+
+
+export const CheckIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+         stroke="currentColor" className="w-4 h-4">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+            d="M5 13l4 4L19 7"/>
+    </svg>
+  )
+}
