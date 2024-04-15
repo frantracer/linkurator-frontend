45c11994a3ae9cb6d461e6a8c28913edc5cc93d0
frantracer <frantracer@gmail.com>
2024-04-15 10:10:58 +0200

    Add menu button in profile page

--- app/(content)/profile/page.tsx
+++ app/(content)/profile/page.tsx
@@ -11,6 +11,9 @@ import TopTitle from "../../../components/molecules/TopTitle";
 import FlexRow from "../../../components/atoms/FlexRow";
 import Avatar from "../../../components/atoms/Avatar";
 import Box from "../../../components/atoms/Box";
+import {showLateralMenu} from "../../../utilities/hideLateralMenu";
+import {LATERAL_NAVIGATION_MENU_ID} from "../../../components/organism/LateralNavigationMenu";
+import {MenuIcon} from "../../../components/atoms/Icons";
 
 const ProfilePage: NextPage = () => {
   const router = useRouter();
@@ -31,7 +34,9 @@ const ProfilePage: NextPage = () => {
   return (
     <main className="min-h-screen bg-base-100">
       <TopTitle>
-        <div></div>
+        <Button clickAction={() => showLateralMenu(LATERAL_NAVIGATION_MENU_ID)} showOnlyOnMobile={true}>
+          <MenuIcon/>
+        </Button>
         <FlexRow>
           {avatarUrl !== "" && <Avatar src={avatarUrl} alt={userName}></Avatar>}
           <h1>{userName}</h1>
