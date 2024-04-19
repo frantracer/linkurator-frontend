dd823c52ebf033ed52e765dc7f0a35237acb5380
frantracer <frantracer@gmail.com>
2024-04-19 19:16:50 +0200

    Fetch profile only if token exists

--- hooks/useProfile.ts
+++ hooks/useProfile.ts
@@ -9,14 +9,37 @@ export type Profile = {
   email: string
 }
 
+function getCookie(name: string): string | undefined {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookie = parts.pop();
+    if (!cookie) {
+      return undefined;
+    }
+    return cookie.split(';').shift();
+  }
+  return undefined;
+}
+
 const fetchProfile = async () => {
-  const { data } = await axios.get<Profile | undefined>(configuration.PROFILE_URL, { withCredentials: true });
-  return {
-    first_name: data?.first_name || '',
-    last_name: data?.last_name || '',
-    avatar_url: data?.avatar_url || '',
-    email: data?.email || '',
-  };
+  const token = getCookie('token');
+  if (!token) {
+    return undefined;
+  }
+
+  try {
+    const { data } = await axios.get<Profile | undefined>(configuration.PROFILE_URL, { withCredentials: true });
+    return {
+      first_name: data?.first_name || '',
+      last_name: data?.last_name || '',
+      avatar_url: data?.avatar_url || '',
+      email: data?.email || '',
+    };
+  } catch (error) {
+    console.error("Error retrieving profile", error);
+    return undefined;
+  }
 };
 
 const useProfile = () => {
