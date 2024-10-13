03e54148b7b7b2337db86b9a75295f6af3530f4b
frantracer <10522656+frantracer@users.noreply.github.com>
2024-10-13 13:24:50 +0200

    Add items position for flex column

--- components/atoms/FlexColumn.tsx
+++ components/atoms/FlexColumn.tsx
@@ -1,14 +1,26 @@
+type Position = "start" | "center" | "end";
+
 type FlexColumnProps = {
+  position?: Position;
   children?: React.ReactNode;
 };
 
 const FlexColumn = (
   {
+    position = "start",
     children
   }: FlexColumnProps
 ) => {
+  const positionMap = {
+    "start": "items-start",
+    "center": "items-center",
+    "end": "items-end"
+  }
+
+  const positionClass = positionMap[position];
+
   return (
-    <div className={`flex flex-col min-w-fit h-full gap-4`}>
+    <div className={`flex flex-col min-w-fit h-full gap-4 ${positionClass}`}>
       {children}
     </div>
   )
