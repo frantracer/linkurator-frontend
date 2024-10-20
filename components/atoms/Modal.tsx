66896116094304d296742e25d72b2755dcb14d8f
frantracer <10522656+frantracer@users.noreply.github.com>
2024-10-20 17:04:50 +0200

    Fix modal to display components outside the modal

--- components/atoms/Modal.tsx
+++ components/atoms/Modal.tsx
@@ -9,7 +9,7 @@ type ModalProps = {
 const Modal = (props: ModalProps) => {
   return (
     <dialog id={props.id} className="bg-black/50 modal modal-bottom sm:modal-middle max-h-screen">
-      <div className="modal-box">
+      <div className="modal-box overflow-y-visible">
         <form tabIndex={0} method="dialog">
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"><CrossIcon/></button>
         </form>
