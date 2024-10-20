5c0a360bdd601843693dda7de6b94afe80c42563
frantracer <10522656+frantracer@users.noreply.github.com>
2024-10-20 17:05:17 +0200

    Replace selector with dropdown in edit topic modal

--- components/organism/EditTopicModal.tsx
+++ components/organism/EditTopicModal.tsx
@@ -4,15 +4,18 @@ import useSubscriptionsToAdd from "../../hooks/useSubscriptionsToAdd";
 import {Subscription} from "../../entities/Subscription";
 import Button from "../atoms/Button";
 import InputText from "../atoms/InputText";
-import Tag from "../atoms/Tag";
 import Modal from "../atoms/Modal";
 import {closeModal} from "../../utilities/modalAction";
-import {CrossIcon} from "../atoms/Icons";
 import Box from "../atoms/Box";
-import Select from "../atoms/Select";
 import FlexRow from "../atoms/FlexRow";
 import FlexColumn from "../atoms/FlexColumn";
 import {Topic} from "../../entities/Topic";
+import Dropdown from "../atoms/Dropdown";
+import {MenuItem} from "../atoms/MenuItem";
+import Menu from "../atoms/Menu";
+import Miniature from "../atoms/Miniature";
+import FlexItem from "../atoms/FlexItem";
+import {CheckCircleIcon, CircleIcon} from "../atoms/Icons";
 
 export const EditTopicModalId = "edit-topic-modal";
 
@@ -25,6 +28,7 @@ type EditTopicModalProps = {
 }
 
 const EditTopicModal = (props: EditTopicModalProps) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [newTopicName, setNewTopicName] = useState(props.topic.name);
   const {
     subscriptionsToAdd,
@@ -32,61 +36,61 @@ const EditTopicModal = (props: EditTopicModalProps) => {
     removeSubscription
   } = useSubscriptionsToAdd(props.subscriptions, props.topic)
 
-  const subscriptionBadges = subscriptionsToAdd.map(s => subscriptionToBadge(s, removeSubscription));
-
-  const onSubscriptionSelected = (subscriptionId: string) => {
-    const subscription = props.subscriptions.find(subscription => subscription.uuid === subscriptionId);
-    if (subscription && !subscriptionsToAdd.map(s => s.uuid).includes(subscription.uuid)) {
-      addSubscription(subscription);
-    }
+  const handleUpdateTopic = () => {
+    updateTopic(props.topic.uuid, newTopicName, subscriptionsToAdd.map(s => s.uuid)).then(
+      () => {
+        props.refreshTopics();
+        props.refreshTopicItems();
+        props.refreshSubscriptions();
+        closeModal(EditTopicModalId);
+      }
+    )
   }
+
+  const subscriptionsMenuItems = props.subscriptions.map(subscription => {
+    const isSelected = subscriptionsToAdd.map(s => s.uuid).includes(subscription.uuid);
+    const handleClick = () => {
+      if (isSelected) {
+        removeSubscription(subscription);
+      } else {
+        addSubscription(subscription);
+      }
+    }
+    return <MenuItem key={subscription.uuid}
+                     selected={false}
+                     onClick={handleClick}>
+      <FlexRow position={"start"}>
+        <Miniature src={subscription.thumbnail} alt={subscription.name}/>
+        {subscription.name}
+        <FlexItem grow={true}/>
+        {isSelected && <FlexItem><CheckCircleIcon/></FlexItem>}
+        {!isSelected && <FlexItem><CircleIcon/></FlexItem>}
+      </FlexRow>
+    </MenuItem>
+  })
 
   return (
     <Modal id={EditTopicModalId}>
       <FlexColumn>
         <h1 className="font-bold text-xl w-full text-center">{"Editar categoría"}</h1>
-        <InputText placeholder="Nombre de la categoría" value={newTopicName} onChange={(value) => setNewTopicName(value)}/>
-        <Box title={"Subscripciones"}>
-          <FlexColumn>
-            <Select title={"Selecciona varias subscripciones"} options={
-              props.subscriptions.map(subscription => {
-                return {key: subscription.uuid, label: subscription.name}
-              })
-            } onChange={(key) => onSubscriptionSelected(key)}/>
-            <FlexRow position={"start"} wrap={true}>
-              {subscriptionBadges}
-            </FlexRow>
-          </FlexColumn>
+        <InputText placeholder="Nombre de la categoría" value={newTopicName}
+                   onChange={(value) => setNewTopicName(value)}/>
+        <Box title={"Subscripciones (" + subscriptionsToAdd.length + ")"}>
+          <Dropdown open={dropdownOpen} onChange={(open) => setDropdownOpen(open)}
+                    start={true} bottom={false}
+                    button={<FlexRow><span>Selecciona varias subscripciones</span></FlexRow>}>
+            <Menu>
+              {subscriptionsMenuItems}
+            </Menu>
+          </Dropdown>
         </Box>
         <FlexRow position={"end"}>
-          <Button clickAction={async () => {
-            updateTopic(props.topic.uuid, newTopicName, subscriptionsToAdd.map(s => s.uuid)).then(
-              () => {
-                props.refreshTopics();
-                props.refreshTopicItems();
-                props.refreshSubscriptions();
-                closeModal(EditTopicModalId);
-              }
-            )
-          }}>
+          <Button clickAction={handleUpdateTopic}>
             <span>{"Editar"}</span>
           </Button>
         </FlexRow>
       </FlexColumn>
     </Modal>
-  )
-}
-
-function subscriptionToBadge(subscription: Subscription, removeSubscription: (subscription: Subscription) => void) {
-  return (
-    <Tag key={subscription.uuid}>
-      <FlexRow>
-        <div onClick={() => removeSubscription(subscription)} className="hover:cursor-pointer">
-          <CrossIcon/>
-        </div>
-        {subscription.name}
-      </FlexRow>
-    </Tag>
   )
 }
 
