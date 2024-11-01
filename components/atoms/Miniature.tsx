a5e8adc178f44fab2817796e0e3e83e4cb9e2aa9
frantracer <10522656+frantracer@users.noreply.github.com>
2024-11-01 14:01:33 +0100

    Increase size for miniatures and tags

--- components/atoms/Miniature.tsx
+++ components/atoms/Miniature.tsx
@@ -6,7 +6,7 @@ type MiniatureProps = {
 const Miniature = (props: MiniatureProps) => {
   return (
     <div className="avatar">
-      <div className="w-4 h-4 rounded bg-neutral">
+      <div className="w-5 h-5 rounded bg-neutral">
         <img src={props.src} alt={props.alt}/>
       </div>
     </div>
--- components/atoms/Tag.tsx
+++ components/atoms/Tag.tsx
@@ -7,7 +7,7 @@ type TagProps = {
 const Tag = (props: TagProps) => {
   return (
     <div className="badge badge-secondary h-fit w-fit max-w-full py-1 text-primary-content justify-start items-center
-   overflow-auto truncate whitespace-nowrap text-xs">
+   overflow-auto truncate whitespace-nowrap text-sm">
       <div className="flex flex-row gap-2 items-center justify-center">
         {props.children}
       </div>
