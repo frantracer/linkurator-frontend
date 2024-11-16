2f55eb65694c7d5701e57b3011675a6e4f22342a
frantracer <10522656+frantracer@users.noreply.github.com>
2024-11-16 18:55:42 +0100

    Set minimum height for menu item

--- components/atoms/MenuItem.tsx
+++ components/atoms/MenuItem.tsx
@@ -18,7 +18,7 @@ export const MenuItem = (props: MenuItemProps) => {
   return (
     <button
       onClick={handleClick}
-      className={`block p-2 text-sm w-full text-left rounded
+      className={`block p-2 text-sm w-full text-left rounded min-h-10
     hover:bg-base-100/80 focus:outline-none focus:shadow-outline ${background}`}>
       {props.children}
     </button>
