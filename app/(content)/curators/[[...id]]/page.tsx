fa779bbd03494f1fb09f62d5df8dd50d70949540
frantracer <frantracer@gmail.com>
2025-10-03 21:39:07 +0200

    Remove image from meta

--- app/(content)/curators/[[...id]]/page.tsx
+++ app/(content)/curators/[[...id]]/page.tsx
@@ -23,13 +23,6 @@ export async function generateMetadata(
     openGraph: {
       title: title,
       description: description,
-      images: [{
-        url: 'https://linkurator.com/logo_v1_medium.png',
-        width: 400,
-        height: 400,
-        alt: 'Linkurator Logo',
-        type: 'image/png',
-      }],
     },
   }
 }
