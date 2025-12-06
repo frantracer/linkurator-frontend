cb21183f854152832cc4e301cc98e696812e6f8b
frantracer <frantracer@gmail.com>
2025-12-06 18:39:16 +0100

    Avoid rendering avatar with empty url

--- components/atoms/Avatar.tsx
+++ components/atoms/Avatar.tsx
@@ -7,7 +7,7 @@ const Avatar = (props: AvatarProps) => {
   return (
     <div className="avatar">
       <div className="w-12 h-12 rounded-md bg-neutral">
-        <img src={props.src} alt={props.alt}/>
+        {props.src && <img src={props.src} alt={props.alt}/>}
       </div>
     </div>
   )
