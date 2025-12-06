52047ae1885d2aca5ad849ec7441125e1b25ccc9
frantracer <frantracer@gmail.com>
2025-12-06 18:41:00 +0100

    Avoid rendering miniatures with empty url

--- components/atoms/Miniature.tsx
+++ components/atoms/Miniature.tsx
@@ -8,7 +8,7 @@ const Miniature = (props: MiniatureProps) => {
   return (
     <div className="avatar">
       <div className="w-5 h-5 rounded">
-        <img src={props.src} alt={props.alt}/>
+        {props.src && <img src={props.src} alt={props.alt}/>}
       </div>
       {props.badgeImage && (
         <div className="absolute bottom-0 right-0 transform translate-x-1/3 translate-y-1/3 w-4 h-4">
