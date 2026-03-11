442952bd1d8a8a91bba579b2d0b4e28c6fd353ea
frantracer <frantracer@gmail.com>
2026-03-11 08:23:47 +0100

    Change modal position to top and center align content

--- components/atoms/Modal.tsx
+++ components/atoms/Modal.tsx
@@ -9,8 +9,8 @@ type ModalProps = {
 
 const Modal = (props: ModalProps) => {
   return (
-    <dialog id={props.id} className="bg-black/50 modal modal-bottom sm:modal-middle max-h-dvh">
-      <div className="modal-box overflow-y-visible w-full max-w-md text-center">
+    <dialog id={props.id} className="bg-black/50 modal modal-top sm:modal-middle max-h-dvh">
+      <div className="modal-box overflow-y-visible w-full max-w-md text-center mx-auto">
         <form method="dialog">
           <button onClick={props.onClose} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
             <CrossIcon/>
