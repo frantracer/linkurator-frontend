beb65632fc8856b640c56f2dc8579c2d7479526b
frantracer <frantracer@gmail.com>
2026-04-21 08:43:12 +0200

    Add border to modal box for improved visibility

--- components/atoms/Modal.tsx
+++ components/atoms/Modal.tsx
@@ -10,7 +10,7 @@ type ModalProps = {
 const Modal = (props: ModalProps) => {
   return (
     <dialog id={props.id} className="bg-black/50 modal modal-top sm:modal-middle max-h-dvh">
-      <div className="modal-box overflow-y-visible w-full max-w-md text-center mx-auto">
+      <div className="modal-box overflow-y-visible w-full max-w-md text-center mx-auto border border-neutral">
         <form method="dialog">
           <button onClick={props.onClose} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
             <CrossIcon/>
