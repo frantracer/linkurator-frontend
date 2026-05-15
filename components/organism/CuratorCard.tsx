d555321d2552710f8b9fd688e90e51c63fec1203
frantracer <frantracer@gmail.com>
2026-05-15 08:56:12 +0200

    Refactor buttons and links on curators cards

--- components/organism/CuratorCard.tsx
+++ components/organism/CuratorCard.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {useRouter} from "next/navigation";
 import {useTranslations} from "next-intl";
 import {paths} from "../../configuration";
 import {Curator} from "../../entities/Curators";
+import ALink from "../atoms/ALink";
 import Button from "../atoms/Button";
-import {AddIcon, MinusIcon} from "../atoms/Icons";
+import CrossButton from "../atoms/CrossButton";
 import Miniature from "../atoms/Miniature";
+import Tag from "../atoms/Tag";
 
 type CuratorCardProps = {
   curator: Curator;
@@ -14,12 +15,7 @@ type CuratorCardProps = {
 }
 
 const CuratorCard = ({curator, onFollow, onUnfollow}: CuratorCardProps) => {
-  const router = useRouter();
   const t = useTranslations("common");
-
-  const handleCardClick = () => {
-    router.push(paths.CURATORS + "/" + curator.username);
-  }
 
   const handleFollow = () => {
     if (onFollow) {
@@ -37,9 +33,12 @@ const CuratorCard = ({curator, onFollow, onUnfollow}: CuratorCardProps) => {
 
   return (
     <div
-      onClick={handleCardClick}
-      className="card rounded-lg w-72 h-full bg-base-200 hover:scale-105 shadow-md border border-neutral hover:shadow-xl hover:border-primary duration-200 cursor-pointer"
+      className="card relative rounded-lg w-72 h-full bg-base-200 hover:scale-105 shadow-md border border-neutral hover:shadow-xl hover:border-primary duration-200 cursor-pointer"
     >
+      <ALink href={paths.CURATORS + "/" + curator.username}>
+        <span className="absolute inset-0"/>
+        <span className="sr-only">{curator.username}</span>
+      </ALink>
       <div className="card-body m-1 p-2 gap-3">
         <div className="flex flex-row items-center gap-2 min-w-0">
           <Miniature src={curator.avatar_url} alt={curator.username}/>
@@ -48,25 +47,18 @@ const CuratorCard = ({curator, onFollow, onUnfollow}: CuratorCardProps) => {
           </h3>
         </div>
         {showActions && (
-          <div className="card-actions flex justify-end mt-auto">
+          <div className="card-actions flex justify-end mt-auto relative z-10">
             {curator.followed ? (
-              <Button
-                primary={false}
-                fitContent={true}
-                clickAction={handleUnfollow}
-                tooltip={t("unfollow")}
-              >
-                <MinusIcon/>
-                {t("unfollow")}
-              </Button>
+              <Tag>
+                <span className="text-xs">{t("following")}</span>
+                <CrossButton onClick={handleUnfollow}/>
+              </Tag>
             ) : (
               <Button
-                primary={false}
+                primary={true}
                 fitContent={true}
                 clickAction={handleFollow}
-                tooltip={t("follow")}
               >
-                <AddIcon/>
                 {t("follow")}
               </Button>
             )}
